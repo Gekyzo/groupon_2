ffe56ce84f588570e04171737642164b70373f28
Ciro Mora <ciro.mora@gmail.com>
2020-03-01 16:45:43 +0100

    Separar tema en elementos

---
+++ plugins/Fashi/templates/element/head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Fashi Template">
+    <meta name="keywords" content="Fashi, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fashi | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <?= $this->Html->css('bootstrap.min') ?>
+    <?= $this->Html->css('font-awesome.min') ?>
+    <?= $this->Html->css('themify-icons') ?>
+    <?= $this->Html->css('elegant-icons') ?>
+    <?= $this->Html->css('owl.carousel.min') ?>
+    <?= $this->Html->css('nice-select') ?>
+    <?= $this->Html->css('jquery-ui.min') ?>
+    <?= $this->Html->css('slicknav.min') ?>
+    <?= $this->Html->css('style') ?>
+
+    <?= $this->fetch('css') ?>
+</head>
---
+++ plugins/Fashi/templates/element/partners.php
@@ -0,0 +1,33 @@
+<!-- Partner Logo Section Begin -->
+<div class="partner-logo">
+    <div class="container">
+        <div class="logo-carousel owl-carousel">
+            <div class="logo-item">
+                <div class="tablecell-inner">
+                    <?= $this->Html->image('logo-carousel/logo-1.png') ?>>
+                </div>
+            </div>
+            <div class="logo-item">
+                <div class="tablecell-inner">
+                    <?= $this->Html->image('logo-carousel/logo-2.png') ?>>
+                </div>
+            </div>
+            <div class="logo-item">
+                <div class="tablecell-inner">
+                    <?= $this->Html->image('logo-carousel/logo-3.png') ?>>
+                </div>
+            </div>
+            <div class="logo-item">
+                <div class="tablecell-inner">
+                    <?= $this->Html->image('logo-carousel/logo-4.png') ?>>
+                </div>
+            </div>
+            <div class="logo-item">
+                <div class="tablecell-inner">
+                    <?= $this->Html->image('logo-carousel/logo-5.png') ?>>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Partner Logo Section End -->
